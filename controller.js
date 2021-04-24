a59e4a833cd4d1f2e5f7d6e17eb5cfd5b597b840
unknown <clappis2@msu.edu>
2021-04-24 15:38:42 -0400

    Adding main files

---
+++ controller.js
@@ -0,0 +1,123 @@
+const todos = require('./data');
+
+/*const findAll = () => {
+	return new Promise((resolve, reject) => {
+		resolve(todos);
+	});
+};*/
+//pull all data
+function findAll(){
+  return new Promise((resolve, reject) => {
+    resolve(todos)
+  })
+}
+
+/*const findById = (id) => {
+	return new Promise((resolve, reject) => {
+		const todo = todos.find((todo) => todo.id === id);
+		if (todo) {
+			resolve(todo);
+		}
+		else {
+			reject(`Todo with id ${id} not found !`);
+		}
+	});
+};*/
+//find specific element
+function findById(id){
+  return new Promise((resolve,reject)=> {
+    const todo = todos.find((p)=>p.oid===id)
+    if(todo){
+          resolve(todos)
+    }
+    else{
+      reject('id '+id+' could not be found')
+    }
+  })
+}
+
+
+/*const deleteById = (id) => {
+	return new Promise((resolve, reject) => {
+		const newTodos = todos.filter((todo) => todo.id !== id);
+		todos = [
+			...newTodos
+		];
+		resolve({ message: 'Todo deleted successfully!!' });
+	});
+};*/
+//delete specific element
+function deleteById(id){
+  return new Promise((resolve,reject)=>{
+    todosRemove = todos.filter((p)=>p.oid !==id)
+    writeDataToFile('data.js',todosRemove)
+    resolve()
+  })
+}
+
+/*const create = (todo) => {
+	return new Promise((resolve, reject) => {
+		const newTodo = {
+			id : Date.now().toString(),
+			...todo
+		};
+		todos = [
+			newTodo,
+			...todos
+		];
+		resolve(newTodo);
+	});
+};*/
+//create new todo
+function create(todo){
+  return new Promise((resolve,reject)=>{
+    const newTodo = {id:Date.now().toString(), ...todo}
+    todos.push(newTodo)
+    writeDataToFile('data.js',todos)
+    resolve(todos)
+  })
+}
+
+/*const updateById = async (id, body) => {
+	try {
+		const { title, description } = body;
+		const todo = await findById(id);
+		if (!todo) {
+			res.writeHead(404, { 'Content-Type': 'application/json' });
+			res.end(JSON.stringify({ message: 'Todo not found!' }));
+		}
+		return new Promise((resolve, reject) => {
+			const updatedTodo = {
+				id,
+				title       :
+					title ? title :
+					todo.title,
+				description :
+					description ? description :
+					todo.description
+			};
+			const index = todos.findIndex((todo) => todo.id === id);
+			todos[index] = updatedTodo;
+			resolve(updatedTodo);
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};*/
+//update data
+function updateById(id, todo){
+  return new Promise((resolve,reject)=>{
+    const index = todos.findIndex((p)=>p.oid===id)
+    todos[index] = {id,...todo}
+    writeDataToFile('data.js',todos)
+    resolve(todos)
+  })
+}
+
+module.exports = {
+	findAll,
+	findById,
+	deleteById,
+	create,
+	updateById
+};
---
+++ server.js
@@ -0,0 +1,36 @@
+//npm init to create pacakge.json
+//npm i -D nodemon to create package lock
+const http = require('http')
+const Todo =  require('./controller')
+const { getPostData } = require('./utils');
+
+
+const server = http.createServer(async (req,res)=>{
+  if(req.url === '/api/todos' && req.method === 'GET'){
+    const todos = await Todo.findAll()
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+		res.end(JSON.stringify(todos))
+  }
+  else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET'){
+    const id = req.url.split('/')[3]
+    const todos = await Todos.findById(id)
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(todos))
+  }
+  else if(req.url === '/api/todos' && req.method === 'POST'){
+
+  }
+  else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT'){
+
+  }
+  else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'DELETE'){
+
+  }
+  else {
+  res.writeHead(404,{'Content-Type' : 'application/json'})
+  res.end(JSON.stringify({message: 'Route Not Found'}))
+  }
+})
+
+const PORT = process.env.PORT || 500
+server.listen(PORT, () => console.log('Server listening on '+PORT))
